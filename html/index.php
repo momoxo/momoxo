13f64567b02025f32f2386b5f33dea9bd5c9be29
hamaco <hamanaka.kazuhiro@gmail.com>
2012-11-18 11:04:31 +0900

    Make 2step installer.

--- html/index.php
+++ html/index.php
@@ -1,17 +1,9 @@
 <?php
-/**
- *
- * @package Xcore
- * @version $Id: index.php,v 1.3 2008/09/25 15:10:27 kilica Exp $
- * @copyright Copyright 2005-2007 XOOPS Cube Project  <https://github.com/momonga-project/momonga>
- * @license https://github.com/momonga-project/momonga/blob/master/docs/GPL_V2.txt GNU GENERAL PUBLIC LICENSE Version 2
- *
- */
-require_once "./mainfile.php";
-require_once "./header.php";
+
+require_once './mainfile.php';
+require_once './header.php';
 
 $xoopsOption['show_cblock'] = 1;
-XCube_DelegateUtils::call("Xcorepage.Top.Access");
+XCube_DelegateUtils::call('Xcorepage.Top.Access');
 
-require_once "./footer.php";
-?>
+require_once './footer.php';
---
+++ xoops_trust_path/installer/config.dist.php
@@ -0,0 +1,19 @@
+<?php echo '<?php' ?>
+
+define('XOOPS_ROOT_PATH', '<?php echo $xoops_root_path ?>');
+define('XOOPS_TRUST_PATH', '<?php echo $xoops_trust_path ?>');
+define('XOOPS_URL', '<?php echo $xoops_url ?>');
+
+define('XOOPS_SALT', '<?php echo $salt ?>');
+
+define('XOOPS_DB_TYPE', 'mysql');
+define('XOOPS_DB_PREFIX', '<?php echo $db_prefix ?>');
+define('XOOPS_DB_HOST', '<?php echo $db_host ?>');
+define('XOOPS_DB_USER', '<?php echo $db_user ?>');
+define('XOOPS_DB_PASS', '<?php echo $db_password ?>');
+define('XOOPS_DB_NAME', '<?php echo $db_name ?>');
+define('XOOPS_DB_PCONNECT', 0);
+
+define('XOOPS_GROUP_ADMIN', '1');
+define('XOOPS_GROUP_USERS', '2');
+define('XOOPS_GROUP_ANONYMOUS', '3');
