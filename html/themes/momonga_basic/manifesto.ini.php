2418c1c555b8717aafeafd2fd9653efa55d2909d
argon <argon.mailbox@gmail.com>
2012-11-18 11:07:48 +0900

    Add Momonga Basic Theme.
    1st commit Momonga Basic Theme.

---
+++ html/themes/momonga_basic/manifesto.ini.php
@@ -0,0 +1,19 @@
+<?php
+/**
+
+[Manifesto]
+Name="Momonga Basic"
+Depends=Legacy_RenderSystem,legacy
+Url="http://www.momonga-project.org/"
+Version="0.1"
+
+[Theme]
+RenderSystem=Legacy_RenderSystem
+Format="XOOPS2 Legacy Style"
+Author=Momonga Project
+ScreenShot="screenshot.png"
+Description="Momonga Basic"
+Licence="GPL"
+
+*/
+?>
