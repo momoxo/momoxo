dcb73ef0174f5c8a1073fcb9bd1f89d502c99db6
hamaco <hamanaka.kazuhiro@gmail.com>
2012-11-18 11:38:35 +0900

    Legacy -> Xcore

--- html/themes/momonga_basic/manifesto.ini.php
+++ html/themes/momonga_basic/manifesto.ini.php
@@ -3,16 +3,16 @@
 
 [Manifesto]
 Name="Momonga Basic"
-Depends=Legacy_RenderSystem,legacy
+Depends=Xcore_RenderSystem,xcore
 Url="http://www.momonga-project.org/"
 Version="0.1"
 
 [Theme]
-RenderSystem="Legacy_RenderSystem"
+RenderSystem="Xcore_RenderSystem"
 Format="XOOPS2 Legacy Style"
 Author="Momonga Project"
 ScreenShot="screenshot.png"
-Description="Momonga Basic"
+Description="Momonga Basic Theme"
 Licence="GPL"
 
 */
