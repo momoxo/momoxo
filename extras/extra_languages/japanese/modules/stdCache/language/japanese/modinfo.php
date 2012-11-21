d7a72e23a9e55daa2e07a602538f0d07b7bcec4f
suin <suinyeze@gmail.com>
2012-11-21 22:28:42 +0900

    remote japanese euc-jp translations. refs #15

--- extras/extra_languages/japanese/modules/stdCache/language/japanese/modinfo.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-define("_MI_STDCACHE_NAME","ɸ�७��å���⥸�塼��");
-define("_MI_STDCACHE_NAME_DESC","ɸ��Ū�ʥ���å���ݥꥷ���� Legacy System �⥸�塼����󶡤��ޤ�");
-
-define("_MI_STDCACHE_BLOCK_CACHECLEAR", "����å��奯�ꥢ�֥�å�");
-
-?>
