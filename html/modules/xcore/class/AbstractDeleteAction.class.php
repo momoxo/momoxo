cb6b84b50f32b18c6153f2d8fda4203c79124649
suin <suinyeze@gmail.com>
2012-11-22 00:30:09 +0900

    html/modules/xcore/classの消してもいいrequire_onceを削除

--- html/modules/xcore/class/AbstractDeleteAction.class.php
+++ html/modules/xcore/class/AbstractDeleteAction.class.php
@@ -1,16 +1,4 @@
 <?php
-/**
- *
- * @package Xcore
- * @version $Id: AbstractDeleteAction.class.php,v 1.3 2008/09/25 15:11:26 kilica Exp $
- * @copyright Copyright 2005-2007 XOOPS Cube Project  <https://github.com/momonga-project/momonga>
- * @license https://github.com/momonga-project/momonga/blob/master/docs/GPL_V2.txt GNU GENERAL PUBLIC LICENSE Version 2
- *
- */
-
-if (!defined('XOOPS_ROOT_PATH')) exit();
-
-require_once XOOPS_MODULE_PATH . "/xcore/class/AbstractEditAction.class.php";
 
 class Xcore_AbstractDeleteAction extends Xcore_AbstractEditAction
 {
@@ -24,5 +12,3 @@ class Xcore_AbstractDeleteAction extends Xcore_AbstractEditAction
 		return $this->mObjectHandler->delete($this->mObject);
 	}
 }
-
-?>
