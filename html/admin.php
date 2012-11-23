8b7e8f18ffd04c0ba2cea5543027f82f1fda98cd
hamaco <hamanaka.kazuhiro@gmail.com>
2012-11-23 22:41:47 +0900

    インストールディレクトリの存在チェック削除

--- html/admin.php
+++ html/admin.php
@@ -18,14 +18,11 @@ class DefaultSystemCheckFunction
 {
 	public static function DefaultCheck()
 	{
-		if (ini_get('register_globals') == 1) {
+		if ( ini_get('register_globals') == 1 ) {
 		    xoops_error(sprintf(_WARNPHPENV,'register_globals','on',_WARNSECURITY),'','warning');
 		}
-		if (is_dir(XOOPS_ROOT_PATH."/install/" )) {
-		    xoops_error(sprintf(_WARNINSTALL2,XOOPS_ROOT_PATH.'/install/'),'','warning');
-		}
-		if ( is_writable(XOOPS_ROOT_PATH."/mainfile.php" ) ) {
-		    xoops_error(sprintf(_WARNINWRITEABLE,XOOPS_ROOT_PATH.'/mainfile.php'),'','warning');
+		if ( is_writable(XOOPS_ROOT_PATH."/config/config.php") ) {
+		    xoops_error(sprintf(_WARNINWRITEABLE,XOOPS_ROOT_PATH.'/config/config.php'),'','warning');
 		}
 	}
 }
