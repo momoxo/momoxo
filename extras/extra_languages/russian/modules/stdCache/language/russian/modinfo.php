eb98111564f786cc6bab875a2e25c3f08a6caa5e
hamaco <hamanaka.kazuhiro@gmail.com>
2012-11-25 22:00:13 +0900

    Remove extras dir

--- extras/extra_languages/russian/modules/stdCache/language/russian/modinfo.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-define("_MI_STDCACHE_NAME","�����������");
-define("_MI_STDCACHE_NAME_DESC","��������� ����������� �������� ����������� � ������ ������� (Xcore).");
-
-define("_MI_STDCACHE_BLOCK_CACHECLEAR", "���� ������� ����");
-
-?>
--- extras/extra_languages/schinese/modules/stdCache/language/schinese/modinfo.php
+++
@@ -1,8 +0,0 @@
-<?php
-// $Id$
-define("_MI_STDCACHE_NAME","����");
-define("_MI_STDCACHE_NAME_DESC","���������Ե�Xcoreϵͳ");
-
-define("_MI_STDCACHE_BLOCK_CACHECLEAR", "�����黺��");
-
-?>
