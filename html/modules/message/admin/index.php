92707130f28e981017cddbe853afa644fb3d1878
hamaco <hamanaka.kazuhiro@gmail.com>
2013-01-08 20:25:26 +0900

    Remove message module.

--- html/modules/message/admin/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-use XCore\Kernel\Root;
-
-require_once '../../../mainfile.php';
-$root = Root::getSingleton();
-$root->mController->executeForward($root->mContext->mModule->getAdminIndex());
-?>
